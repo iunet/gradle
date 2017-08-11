d5211af90c7270b2cd7067504a1527ec51b3321f
Stefan Oehme <stefan@gradle.com>
2017-08-11 19:22:39 +0200

    Make zipTrees faster
    zipTrees were unpacked every time they were visited,
    which meant 2-4 times for every single build.
    
    This changes the logic to snapshot the zip and only unpack
    again if it has changed.

--- subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeServices.java
+++ subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeServices.java
@@ -296,7 +296,8 @@ public class BuildScopeServices extends DefaultServiceRegistry {
             get(PluginRepositoryRegistry.class),
             get(PluginRepositoryFactory.class),
             get(ProviderFactory.class),
-            get(TextResourceLoader.class));
+            get(TextResourceLoader.class),
+            get(FileHasher.class));
     }
 
     protected SettingsLoaderFactory createSettingsLoaderFactory(SettingsProcessor settingsProcessor, NestedBuildFactory nestedBuildFactory,
